ba378501fe7ffd4ff958c006d7c195f05ed73b1b
전대현 <detail54@nate.com>
2021-11-10 22:46:20 +0900

    chore: Add link tag css

--- src/common/link/LinkTag.styles.ts
+++ src/common/link/LinkTag.styles.ts
@@ -11,22 +11,27 @@ const LinkTag = styled(Link)`
   text-decoration: none;
   color: white;
   ${(props: ILinkStyles) =>
-    props.fontSize && setStyles.setFontSize(props.fontSize)}
+    props.fontSize && setStyles.setFontSize(props.fontSize)};
   ${(props: ILinkStyles) =>
-    props.fontWeight && setStyles.setFontWeight(props.fontWeight)}
+    props.fontWeight && setStyles.setFontWeight(props.fontWeight)};
+  ${(props: ILinkStyles) => props.alignX && setStyles.setAlignX(props.alignX)};
+  ${(props: ILinkStyles) => props.alignY && setStyles.setAlignY(props.alignY)};
+  ${(props: ILinkStyles) =>
+    (props.width || props.height) &&
+    setStyles.setWidthAndHeight(props.width, props.height)};
 
   ${(props: ILinkStyles) =>
     props.opacity &&
     css`
       opacity: 0.6;
-    `}
+    `};
   ${(props: ILinkStyles) =>
     (props.hoverColor || props.hoverBgColor || props.hoverOpacity) &&
     setStyles.setHoverEffect(
       props.hoverColor,
       props.hoverBgColor,
       props.hoverOpacity,
-    )}
+    )};
 `
 
 export const LinkSC = {
